5c2c46f9d084754179365fc7a8bab7d689c05bb8
Carlos Passos <carlos@softcap.com.br>
2021-02-28 15:50:30 -0300

    first commit

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -6,11 +6,10 @@ export default NextAuth({
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
-    }), 
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
     // ...add more providers here
   ],
-
   // A database is optional, but required to persist accounts in a database
   database: process.env.DATABASE_URL,
 })
